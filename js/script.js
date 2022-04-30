f7ae6b2687f6948b1318106dba272a3fd6641295
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-04-30 11:47:09 +0200

    answer

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("#contactForm");
+
+form.onsubmit = function () {
+  event.preventDefault();
+
+  console.log(event);
+
+  const name = document.querySelector("#name");
+  console.log(name.value);
+};
